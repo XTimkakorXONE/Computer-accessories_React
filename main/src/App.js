80793f6b16a288914fc0405db7267998c695b3b5
Artem Korolev <korolevartem848@gmail.com>
2022-10-17 20:41:07 +0300

    work with props

--- main/src/App.js
+++ main/src/App.js
@@ -3,6 +3,29 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/CartDrawer";
 
+const arr = [
+  {
+    title: "Hyperx cloud II Wirelles",
+    price: "10 512",
+    img: "/img/headphones1.webp",
+  },
+  {
+    title: "Mucai 24D 240Hz",
+    price: "18 000",
+    img: "/img/monitor.webp",
+  },
+  {
+    title: "logitech g pro x superlight",
+    price: "10 000",
+    img: "/img/logitech.jpg",
+  },
+  {
+    title: "HyperX Alloy Origins 60",
+    price: "6499",
+    img: "/img/HXAO60.webp",
+  },
+];
+
 function App() {
   return (
     <div className="wrapper">
@@ -20,7 +43,14 @@ function App() {
           </div>
         </div>
         <div className="sneakers">
-          <Card />
+          {arr.map((obj) => (
+            <Card
+              title={obj.title}
+              price={obj.price}
+              img={obj.img}
+              onClick={() => console.log(obj)}
+            />
+          ))}
         </div>
       </div>
     </div>
--- main/src/components/Card.js
+++ main/src/components/Card.js
@@ -1,20 +1,21 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
+  console.log(props);
   return (
     <div className="card">
       <div className="favorite">
         <img src="/img/heart.svg" alt="heart" />
       </div>
-      <img src="/img/headphones1.webp" alt="Hyperx" width={133} height={123} />
-      <h5>Hyperx cloud II Wirelles</h5>
+      <img src={props.img} alt="Hyperx" width={133} height={123} />
+      <h5>{props.title}</h5>
       <div className="cardBottom">
         <div className="cost">
           <span>Цена:</span>
           <br></br>
-          <b>12 999 руб.</b>
+          <b>{props.price}</b>
         </div>
-        <button className="button">
+        <button className="button" onClick={props.onClick}>
           <img width={21} height={21} src="/img/add.png" alt="plus" />
         </button>
       </div>
